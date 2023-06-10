dbc4c40752d5ceb0f68bf3527eb8b97f071856ff
ThOM HO <thom1622000@gmail.com>
2023-06-10 18:53:06 +0700

    update cart page

--- app/scripts/cart-service.js
+++ app/scripts/cart-service.js
@@ -1,14 +1,37 @@
 export function updateInfoCartPage(data, lineIndex) {
     const div = document.createElement("div");
     div.innerHTML = data;
-    const ulElement = document.querySelector(`ul.cart__item.jsLineItem[data-line-index="${lineIndex}"]`);
-    const ulElementNew = div.querySelector(`ul.cart__item.jsLineItem[data-line-index="${lineIndex}"]`);
-    const jsLineUpdatesOld = ulElement.querySelectorAll(".jsLineUpdate");
-    const jsLineUpdatesNew = ulElementNew.querySelectorAll(".jsLineUpdate");
+    const liElement = document.querySelector(`li.cart__item.jsLineItem[data-line-index="${lineIndex}"]`);
+    const liElementNew = div.querySelector(`li.cart__item.jsLineItem[data-line-index="${lineIndex}"]`);
+    const inputElementNew = liElementNew.querySelector('.quantity__input');
+    const jsLineUpdatesOld = liElement.querySelectorAll(".jsLineUpdate");
+    const jsLineUpdatesNew = liElementNew.querySelectorAll(".jsLineUpdate");
 
     jsLineUpdatesOld.forEach((item, index) => {
-        item.replaceWith(jsLineUpdatesNew[index]);
+        item.parentNode.replaceChild(jsLineUpdatesNew[index], item);
     });
 
-    return ulElement;
+    return inputElementNew;
+}
+
+
+export function checkMaxCartPage() {
+    const listItemCart = document.querySelectorAll(".jsLineItem");
+    listItemCart.forEach(line => {
+        const input = line.querySelector('.quantity__input')
+        const add__qlt = line.querySelector('.add__qlt')
+        checkMaxLine(input, add__qlt);
+    })
+}
+
+export function checkMaxLine(elmInput, elmAdd) {
+    const currentValue = parseInt(elmInput.getAttribute('value'))
+    const maxValue = parseInt(elmInput.getAttribute('max'))
+    if (currentValue >= maxValue) {
+        elmAdd.setAttribute('disabled', '')
+    } else {
+        if (elmAdd.hasAttribute('disabled')) {
+            elmAdd.removeAttribute('disabled')
+        }
+    }
 }
--- app/scripts/cart.js
+++ app/scripts/cart.js
@@ -1,25 +1,43 @@
-import { updateInfoCartPage } from "./cart-service";
-import { fetchAPIUpdateItemCart, shopifyReloadSection } from "./utils";
+import { event } from "jquery";
+import { checkMaxCartPage, checkMaxLine, updateInfoCartPage } from "./cart-service";
+import { debounce, fetchAPIUpdateItemCart, shopifyReloadSection } from "./utils";
 const sectionId = document.querySelector('.cart-section-wrapper').dataset.sectionId
 shopifyReloadSection(init, sectionId)
 function init() {
     let removeBtns = document.querySelectorAll('.cart__item .remove__qlt');
     let addBtns = document.querySelectorAll('.cart__item .add__qlt');
 
-    async function handleAddClick(type, target, event) {
+    async function handleClickButton(type, target, event) {
         event.preventDefault();
         const lineIndex = target.closest('.cart__item.jsLineItem').dataset.lineIndex;
         const quantityInput = document.querySelector(`.cart__item.jsLineItem[data-line-index="${lineIndex}"] .quantity__input`);
+        const elmAdd = document.querySelector(`.cart__item.jsLineItem[data-line-index="${lineIndex}"] .add__qlt`);
         const sectionId = quantityInput.getAttribute('data-sections');
-        const variantId = quantityInput.getAttribute('data-key').split(":")[0];
+        const variantId = quantityInput.getAttribute('data-key');
         let newQuantity = 0;
         switch (type) {
             case 'add':
-                newQuantity = parseInt(quantityInput.getAttribute('value')) + 1;
+                newQuantity = parseInt(quantityInput.value) + 1;
                 break;
             case 'remove':
-                newQuantity = parseInt(quantityInput.getAttribute('value')) - 1;
+                newQuantity = parseInt(quantityInput.value) - 1;
                 break;
+            case 'change':
+                console.log("abc")
+                const max = parseInt(quantityInput.getAttribute("max"))
+                const min = parseInt(quantityInput.getAttribute("min"))
+                newQuantity = parseInt(quantityInput.value);
+                console.log("abcd", newQuantity)
+
+                if (newQuantity > max) {
+                    newQuantity = max
+                }
+
+                if (newQuantity < min) {
+                    newQuantity = min
+                }
+
+                break
             default:
                 break;
         }
@@ -28,17 +46,43 @@ function init() {
             newQuantity: newQuantity,
             sectionId: sectionId
         };
-        const data = await fetchAPIUpdateItemCart(options)
-        const result = data.sections[sectionId]
+        console.log("options", options);
 
-        updateInfoCartPage(result, lineIndex);
+        try {
+            const data = await fetchAPIUpdateItemCart(options)
+            const result = data.sections[sectionId]
+            const inputElementNew = updateInfoCartPage(result, lineIndex);
+            checkMaxLine(inputElementNew, elmAdd)
+            inputEvents()
+
+        } catch (error) {
+
+        }
+
+
     }
 
     addBtns.forEach(function (addBtn) {
-        addBtn.addEventListener('click', (event) => handleAddClick('add', event.target, event));
+        addBtn.addEventListener('click', (event) => handleClickButton('add', event.target, event));
     });
 
     removeBtns.forEach(function (removeBtn) {
-        removeBtn.addEventListener('click', (event) => handleAddClick('remove', event.target, event));
+        removeBtn.addEventListener('click', (event) => handleClickButton('remove', event.target, event));
     });
+
+    function inputEvents() {
+        let inputs = document.querySelectorAll('.cart__item .quantity__input');
+        inputs.forEach(function (input) {
+            input.addEventListener('keydown', function (event) {
+                if (event.key === 'Enter') {
+                    event.preventDefault();
+                }
+            })
+            input.addEventListener('input', debounce((event) => handleClickButton('change', event.target, event), 2000))
+        })
+    }
+
+    inputEvents()
+
+    checkMaxCartPage()
 }
