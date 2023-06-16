fb1224310365e0fb597c4eb02bf597446708edbc
ThOM HO <thom1622000@gmail.com>
2023-06-16 14:54:02 +0700

    change code collection

--- app/scripts/product-description.js
+++ app/scripts/product-description.js
@@ -1,27 +1,50 @@
+const headerList = document.querySelectorAll('.jsNavTab');
+const headerList1 = document.querySelectorAll('.jsNavTab1');
+const bodyList = document.querySelectorAll('.jsBodyTab');
+const bodyList1 = document.querySelectorAll('.jsBodyTab1');
 
-const headerList = document.querySelectorAll('.jsNavTab')
-const bodyList = document.querySelectorAll('.jsBodyTab')
-
-function createActive(header, bodyList, index) {
-    const body = bodyList[index];
+function createActive(id) {
+    const targetElement = document.querySelector(`.jsBodyTab[data-id="${id}"]`);
+    const targetHeader = document.querySelector(`.jsNavTab[data-id="${id}"]`);
     document.querySelector('.jsNavTab.active')?.classList.remove("active");
-    header.classList.add("active");
+    targetHeader?.classList.add("active");
     document.querySelector('.jsBodyTab.active')?.classList.remove("active");
-    body?.classList.add("active");
+    targetElement?.classList.add("active");
 }
 
-if (headerList) {
-    const initIndex = 0;
-    const header = headerList[initIndex]
-    createActive(header, bodyList, initIndex)
-
-    headerList.forEach((header, index) => {
-        header.addEventListener("click", () => createActive(header, bodyList, index))
-
-    })
-
+function createActive1(id) {
+    const targetElement = document.querySelector(`.jsBodyTab1[data-id="${id}"]`);
+    const targetHeader = document.querySelector(`.jsNavTab1[data-id="${id}"]`);
+    if (targetHeader.classList.contains('active')) {
+        targetElement.classList.remove("active");
+        targetHeader.classList.remove("active");
+    } else {
+        targetHeader.classList.add("active");
+        targetElement.classList.add("active");
+    }
 }
 
-import { tns } from "tiny-slider";
 
+if (headerList.length > 0) {
+    const idFirstHeader = headerList[0].dataset.id;
+    createActive(idFirstHeader);
+
+    headerList.forEach((header) => {
+        header.addEventListener("click", () => {
+            const id = header.dataset.id;
+            createActive(id);
+        });
+    });
+}
+
+
+if (headerList1.length > 0) {
+    console.log('1')
+    headerList1.forEach((header) => {
+        header.addEventListener("click", () => {
+            const id = header.dataset.id;
+            createActive1(id);
+        });
+    });
+}
 
